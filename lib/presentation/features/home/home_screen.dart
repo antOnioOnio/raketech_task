751a0cd931753397c33c9f489139b6bf3c5b5baf
Antonio Garcia <antonio.garciac@externos.correo.gob.es>
2024-01-27 20:49:24 +0100

    first approach to tabbar

--- lib/presentation/features/home/home_screen.dart
+++ lib/presentation/features/home/home_screen.dart
@@ -2,20 +2,60 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:raketech_task/app/constants/app_assets.dart';
 import 'package:raketech_task/app/constants/app_colors.dart';
-import 'package:raketech_task/app/extensions/context_extensions.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: AppColors.primaryWhite,
-      body: SafeArea(
-        child: Column(
+    return DefaultTabController(
+      length: 3,
+      initialIndex: 0,
+      child: Scaffold(
+        backgroundColor: AppColors.primaryWhite,
+        appBar: AppBar(
+          backgroundColor: AppColors.primary,
+          title: SvgPicture.asset(
+            AppAssets.appLogo,
+            height: 44,
+          ),
+          centerTitle: true,
+        ),
+        body: Column(
           children: [
-
-
+            TabBar(
+              /*          labelStyle: AFTheme.of(context).typoOnLight.bodyMdBold,
+            unselectedLabelStyle: AFTheme.of(context).typoOnLight.bodyMdBold,*/
+              labelColor: AppColors.complementary,
+              unselectedLabelColor: AppColors.disableBlack,
+              tabs: [
+                Tab(
+                  child: Text('YESTERDAY'),
+                ),
+                Tab(
+                  child: Text('TODAY'),
+                ),
+                Tab(
+                  child: Text('TOMORROW'),
+                ),
+              ],
+              onTap: (newTabIndex) => DoNothingAction(),
+            ),
+            Expanded(
+              child: TabBarView(
+                children: [
+                  Container(
+                    color: Colors.pinkAccent,
+                  ),
+                  Container(
+                    color: Colors.red,
+                  ),
+                  Container(
+                    color: Colors.blue,
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
